cd4bc53d031ee2ff4ca8671f08ae14039c477aea
paolopaolopaolo <dpaolomercado@gmail.com>
2015-09-03 14:39:24 -0700

    Configuration Stuff Added:
    - Some real quick configs to add to the mix

--- j_app/settings.py
+++ j_app/settings.py
@@ -17,7 +17,10 @@ except ImportError:
     # SECURITY WARNING: don't run with debug turned on in production!
     DEBUG = False
     TEMPLATE_DEBUG = False
-    ALLOWED_HOSTS = []
+    ALLOWED_HOSTS = [
+                        '.journelope.com.',
+                        '.journelope.com'
+                    ]
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -54,13 +57,15 @@ if DEBUG:
     }
     
 else:
+    # with open(os.path.join(BASE_DIR, 'SECRET_KEY.txt'), 'rb') as secret_key:
+        # SECRET_KEY = secret_key.read()
     SECRET_KEY = os.environ['JAPP_SK']
 
     # Database
     # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.mysql',
+            'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
@@ -88,6 +93,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mod_wsgi.server',
     'compressor',
     'journalapp'
 )
