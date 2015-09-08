5e792e1249d734e76d6d6b675a31820d8cf38bb6
paolopaolopaolo <dpaolomercado@gmail.com>
2015-09-08 15:10:44 -0700

    Styling and Spacing Fixes:
    - Hover affect re-enabled on journal items
    - Journal items spaced out a bit

--- static/j_app/journal/js/backbone/views/JournalPageView.js
+++ static/j_app/journal/js/backbone/views/JournalPageView.js
@@ -12,15 +12,17 @@ var JournalPageView = Backbone.View.extend({
 	_toggleSideBarCollapse: function (event) {
 		if (!this.side_bar_collapse) {
 			this.$el.find('.sidebar').addClass('vert-collapsed');
-			this.$el.find('.sidebar').find('i')
-									 .addClass('fa-chevron-right')
-									 .removeClass('fa-chevron-left');
+			this.$el.find('.side-caret')
+					.find('i')
+					.addClass('fa-chevron-right')
+					.removeClass('fa-chevron-left');
 			this.$el.find('.journal-view').hide();
 		} else {
 			this.$el.find('.sidebar').removeClass('vert-collapsed');
-			this.$el.find('.sidebar').find('i')
-									 .removeClass('fa-chevron-right')
-									 .addClass('fa-chevron-left');
+			this.$el.find('.side-caret')
+					.find('i')
+					.removeClass('fa-chevron-right')
+					.addClass('fa-chevron-left');
 		 	this.$el.find('.journal-view').show();
 		}
 		this.side_bar_collapse = !this.side_bar_collapse;
